d62ef85f349c81ee417fd995adbb74e25d3403a5
yuchilampython <101412850+yuchilampython@users.noreply.github.com>
2022-06-14 00:20:58 +0800

    base setting

--- Authentication/diaryapp/diaryapp/asgi.py
+++ Authentication/diaryapp/diaryapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for authentication project.
+ASGI config for diaryapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authentication.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diaryapp.settings')
 
 application = get_asgi_application()
