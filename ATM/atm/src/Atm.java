a84684733bd84134f5ba1ffb2c3efbcb3db312d4
yuchilampython <101412850+yuchilampython@users.noreply.github.com>
2022-06-16 23:47:52 +0800

    Withdrawal function added

--- ATM/atm/src/Atm.java
+++ ATM/atm/src/Atm.java
@@ -34,14 +34,12 @@ public class Atm {
         Account account = new Account();
         
         while(true){
-            try{
-                System.out.println("Please input your full name: ");
-                String name = sc.next();
-                account.setName(name);
-                break;
-            } catch (Exception e){
-                System.out.println("Please enter a valid full name.");
-            }
+
+            System.out.println("Please input your full name: ");
+            String name = sc.next();
+            account.setName(name);
+            break;
+
         }
         
         while(true){
@@ -49,7 +47,8 @@ public class Atm {
             System.out.println("(Password must contains number, uppercase character, lowercase chacter");
             System.out.println("and at least 8 characters long)");
             String password = sc.next();
-            String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+            //String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+            String pattern = "[a-z]";
             // check if the password match the password policy
             if (password.matches(pattern)){
                 System.out.println("Please re-enter your password: ");
@@ -67,13 +66,15 @@ public class Atm {
         }
         
         while(true){
+            System.out.println("Please enter the withdrawal limit of each transaction:");
+
             try {
-                System.out.println("Please enter the withdrawl limit of each transaction:");
                 Integer limit = sc.nextInt();
                 account.setLimit(limit);
                 break;
             } catch (Exception e) {
-                System.out.println("Please enter an Integer.");
+                String bad_input = sc.nextLine();
+                System.out.println("Please enter a valid integer.\n");
             }
         }
 
@@ -131,11 +132,101 @@ public class Atm {
         Account getAccount = getAccountByCardNumber(cardNumber, accountList);
         if (getAccount != null && getAccount.getPassword().equals(password)){
             System.out.println("Login successful \n");
-            String name = getAccount.getName();
-            System.out.println("=====Welcome, " + name + "=====");
-            System.out.println("Please enter the corresponding number to proceed:");
-        } else{
+            loggedInMenu(getAccount, sc);
+
+        } else {
             System.out.println("User not exists or password is wrong.\n");
+        }
+    }
+
+    private static void loggedInMenu(Account account, Scanner sc) {
+        String name = account.getName();
+        System.out.println("\n=====Welcome, " + name + "=====");
+        System.out.println("Please enter the corresponding number to proceed:");
+        System.out.println("1. Account inquiry");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdrawal");
+        System.out.println("4. Transfer");
+        System.out.println("5. Change password");
+        System.out.println("6. Quit");
+        System.out.println("7. Cancel account");
+        System.out.println("Please enter the corresponding number to proceed: ");
+        Integer command = sc.nextInt();
+        switch (command) {
+            case 1: // account inquiry
+                accountInquiry(account, sc);
+            case 2: // deposit
+                deposit(account, sc);
+            case 3: // withdrawal
+                withdrawal(account, sc);
+        }
+    }
+
+    private static void accountInquiry(Account account, Scanner sc){
+        System.out.println("\n=====Account inquiry=====");
+        System.out.println("Name: " + account.getName());
+        System.out.println("Card Number: " + account.getCardNumber());
+        System.out.println("Balance: " + account.getBalance());
+        System.out.println("Withdrawal Limit: " + account.getLimit());
+        backToMenu(account, sc);
+    }
+
+    private static void backToMenu(Account account, Scanner sc){
+        while(true) {
+            System.out.println("Please enter '1' to go back to the previous page");
+            try {
+                Integer command = sc.nextInt();
+                if (command == 1) {
+                    loggedInMenu(account, sc);
+                    break;
+                }
+            } catch (Exception e) {
+                    String bad_input = sc.nextLine();
+            }
+        }
+    }
+
+    private static void deposit(Account account, Scanner sc) {
+        while(true) {
+            System.out.println("Input the amount to be deposit: ");
+            try {
+                Integer amount = sc.nextInt();
+                Double balance = account.getBalance() + amount;
+                account.setBalance(balance);
+                System.out.println("You have deposited " + amount + " dollars.");
+                backToMenu(account, sc);
+            } catch (Exception e) {
+                String bad_input = sc.nextLine();
+                System.out.println("The input amount is wrong, please input again.");
+            }
+        }
+    }
+
+    private static void withdrawal(Account account, Scanner sc) {
+        System.out.println("Input the amount of money you would like to withdraw: ");
+        try {
+            Integer amount = sc.nextInt();
+
+            // check the balance
+            if (account.getBalance() > amount){
+                // check the withdrawal limit
+                if (account.getLimit() > amount) {
+                    // change the money in the balance
+                    Double current_balance = account.getBalance();
+                    account.setBalance(current_balance - amount);
+                    System.out.println("You have taken out " + amount + " dollars");
+                    backToMenu(account, sc);
+                } else {
+                    System.out.println("Your withdrawal limit is lower than the money you want to get.");
+                    backToMenu(account, sc);
+                }
+            } else {
+                System.out.println("You do not have enough money.");
+                backToMenu(account, sc);
+            }
+        } catch (Exception e) {
+            String bad_input = sc.nextLine();
+            System.out.println("Please enter an integer.");
         }
     }
 }
